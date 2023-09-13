a394a28e43661b699750db14d5a4fe2b91a4042b
ujujungbuck <garam8u2@gmail.com>
2023-09-13 23:39:27 +0900

    Feat : Link에 active 적용

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -35,7 +35,7 @@ export default function NavBar() {
           font-size: 18px;
         }
         .active {
-          color: gray;
+          color: tomato;
         }
         nav div {
           display: flex;
